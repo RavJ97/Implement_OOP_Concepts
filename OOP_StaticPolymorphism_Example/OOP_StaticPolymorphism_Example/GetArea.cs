113788184f681010ade1026856f92c006490e154
RavJ97 <rwm.ravindu@gmail.com>
2019-07-07 11:50:13 +0530

    added static polymorphism example

---
+++ OOP_StaticPolymorphism_Example/OOP_StaticPolymorphism_Example/GetArea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_StaticPolymorphism_Example
+{
+    class GetArea
+    {
+        //In static polymorphism there can be "n" number of methods with the same name, but each should have a different set of parameters
+        public void getArea(int x)
+        {
+            Console.WriteLine("The Area of the Square : " + x * x);
+        }
+        public void getArea(int x, int y)
+        {
+            Console.WriteLine("The Area of the Rectangle : " + x * y);
+        }
+    }
+}
---
+++ OOP_StaticPolymorphism_Example/OOP_StaticPolymorphism_Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_StaticPolymorphism_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Object created to call the methods in GetArea class
+            GetArea ga = new GetArea();
+
+            //Calling the methods using the object created
+            ga.getArea(5);
+            ga.getArea(10,5);
+
+            Console.ReadLine();
+        }
+    }
+}
