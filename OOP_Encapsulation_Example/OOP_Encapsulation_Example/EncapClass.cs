ed35258c8a1166fe19f0720635b366ae60233386
RavJ97 <rwm.ravindu@gmail.com>
2019-07-07 11:03:13 +0530

    added encapsulation example

---
+++ OOP_Encapsulation_Example/OOP_Encapsulation_Example/EncapClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Encapsulation_Example
+{
+    class EncapClass
+    {
+        private int age;
+        private string name;
+
+        //Setting age variable from program class into the private variable in EncapClass using a public method
+        public void setAge(int userAge)
+        {
+            age = userAge;
+        }
+
+        //Returning the private variable age to the program class using a public method
+        public int getAge()
+        {
+            return age;
+        }
+
+        //Setting name variable from program class into the private variable in EncapClass using a public method
+        public void setName(string userName)
+        {
+            name = userName;
+        }
+
+        //Returning the private variable name to the program class using a public method
+        public string getName()
+        {
+            return name;
+        }
+    }
+}
---
+++ OOP_Encapsulation_Example/OOP_Encapsulation_Example/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Encapsulation_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable declaration
+            int age;
+            string name;
+
+            //Object made to call the Encapsulation Class
+            EncapClass ec = new EncapClass();
+
+            //User inputs taken for name and age
+            Console.WriteLine("Enter Your Name ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Your Age ");
+            age = int.Parse(Console.ReadLine());
+
+            //Setting the name and age variable to the private variables in the Encapsulation Class
+            //Calling the methods using the object created to set the values to the private variables
+            ec.setAge(age);
+            ec.setName(name);
+
+            //Printing the private variables in Encapsulation Class
+            //Calling the methods using the object created to get the values from the private variables
+            Console.WriteLine("Your Name is " + ec.getName());
+            Console.WriteLine("Your Age is " + ec.getAge());
+
+            Console.ReadLine();
+        }
+    }
+}
