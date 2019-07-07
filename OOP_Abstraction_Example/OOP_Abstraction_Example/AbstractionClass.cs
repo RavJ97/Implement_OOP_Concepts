759b96eb216a1f57e9187388af0ecb2c66506646
RavJ97 <rwm.ravindu@gmail.com>
2019-07-07 12:30:52 +0530

    added abstraction example

---
+++ OOP_Abstraction_Example/OOP_Abstraction_Example/AbstractionClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Abstraction_Example
+{
+    abstract class AbstractionClass
+    {
+        //abstract methods can only be defined in the abstract class (can't add body here)
+        public abstract int MultiplyNumbers(int n1, int n2);
+
+        //Abstract classes can also have normal methods
+        public int AddTwoNumbers(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
---
+++ OOP_Abstraction_Example/OOP_Abstraction_Example/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Abstraction_Example
+{
+    class Program : AbstractionClass
+    {
+        static void Main(string[] args)
+        {
+            //Creating the object for the child class
+            Program pm = new Program();
+
+            //Calling the normal method using the object created
+            Console.WriteLine("The Addition is " + pm.AddTwoNumbers(10, 20));
+
+            //Calling the abstract method using the object created
+            Console.WriteLine("The Multiplication is " + pm.MultiplyNumbers(10, 20));
+
+            Console.ReadLine();
+        }
+
+        //The body of the abstract method should be coded in the class where the abstract method will be called
+        public override int MultiplyNumbers(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+    }
+}
